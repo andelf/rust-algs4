4b1f82744fe85f294fdb3f260f6c367946b30a15
andelf <andelf@gmail.com>
2015-08-19 01:41:02 +0800

    maximum_flow: implement Ford-Fulkerson algorithm for FlowNetwork

--- src/maximum_flow/mod.rs
+++ src/maximum_flow/mod.rs
@@ -1,11 +1,13 @@
 use std::fmt;
 use std::iter;
+use std::f64;
+use std::sync::{Arc, RwLock};
 
 use adivon::bag::Bag;
+use adivon::queue::Queue;
 
 
 /// Capacitated edge with a flow in a flow network.
-#[derive(Clone, Copy)]
 pub struct FlowEdge {
     v: usize,                   // from
     w: usize,                   // to
@@ -14,7 +16,7 @@ pub struct FlowEdge {
 }
 
 impl FlowEdge {
-    pub fn new_zero_flow(v: usize, w: usize, capacity: f64) -> FlowEdge {
+    pub fn new(v: usize, w: usize, capacity: f64) -> FlowEdge {
         assert!(capacity >= 0.0, "edge capacity must be non-negative");
         FlowEdge {
             v: v,
@@ -24,7 +26,7 @@ impl FlowEdge {
         }
     }
 
-    pub fn new(v: usize, w: usize, capacity: f64, flow: f64) -> FlowEdge {
+    pub fn new_with_flow(v: usize, w: usize, capacity: f64, flow: f64) -> FlowEdge {
         assert!(capacity >= 0.0, "edge capacity must be non-negative");
         assert!(flow <= capacity, "flow exceeds capacity");
         assert!(flow >= 0.0, "flow must be non-negative");
@@ -93,7 +95,7 @@ impl fmt::Debug for FlowEdge {
 
 #[test]
 fn test_flow_edge() {
-    let e = FlowEdge::new_zero_flow(12, 23, 3.14);
+    let e = FlowEdge::new(12, 23, 3.14);
     assert_eq!("12 -> 23 0/3.14", format!("{:?}", e));
 }
 
@@ -101,7 +103,7 @@ fn test_flow_edge() {
 pub struct FlowNetwork {
     v: usize,
     e: usize,
-    adj: Vec<Bag<FlowEdge>>
+    adj: Vec<Bag<Arc<RwLock<FlowEdge>>>>
 }
 
 impl FlowNetwork {
@@ -130,22 +132,215 @@ impl FlowNetwork {
         let w = e.to();
         self.validate_vertex(v);
         self.validate_vertex(w);
-        self.adj[v].add(e);
-        self.adj[w].add(e);
+        let edge = Arc::new(RwLock::new(e));
+        self.adj[v].add(edge.clone());
+        self.adj[w].add(edge);
         self.e += 1;
     }
 
-    pub fn adj(&self, v: usize) -> ::std::vec::IntoIter<FlowEdge> {
+    pub fn adj<'r>(&'r self, v: usize) -> Vec<Arc<RwLock<FlowEdge>>> {
         self.validate_vertex(v);
-        self.adj[v].iter().map(|e| e.clone()).collect::<Vec<FlowEdge>>().into_iter()
+        self.adj[v].iter().map(|e| e.clone()).collect()
     }
 
-    pub fn edges(&self) -> ::std::vec::IntoIter<FlowEdge> {
+    pub fn edges<'r>(&'r self) -> Vec<Arc<RwLock<FlowEdge>>> {
         self.adj.iter()
             .flat_map(|adj| {
-                adj.iter().map(|e| e.clone()).collect::<Vec<FlowEdge>>().into_iter()
+                adj.iter()
+                    .map(|e| e.clone())
+                    .collect::<Vec<Arc<RwLock<FlowEdge>>>>()
+                    .into_iter()
             })
-            .collect::<Vec<FlowEdge>>()
-            .into_iter()
+            .collect()
     }
+
+    pub fn to_dot(&self) -> String {
+        let mut dot = String::new();
+
+        dot.push_str("digraph G {\n");
+        for i in 0 .. self.v {
+            dot.push_str(&format!("  {};\n", i));
+        }
+
+        for v in 0 .. self.v() {
+            for e in self.adj(v) {
+                let e = e.read().unwrap();
+                if v == e.from() {
+                    let w = e.to();
+                    dot.push_str(&format!("  {} -> {} [ label=\"{}/{}\" ];\n",
+                                          v, w, e.flow, e.capacity));
+                }
+            }
+        }
+        dot.push_str("}\n");
+        dot
+    }
+}
+
+/// Ford-Fulkerson algorithm for computing a max flow and
+/// a min cut using shortest augmenting path rule.
+pub struct FordFulkerson<'g> {
+    graph: &'g FlowNetwork,
+    marked: Vec<bool>,
+    edge_to: Vec<Option<Arc<RwLock<FlowEdge>>>>,
+    value: f64
+}
+
+impl<'g> FordFulkerson<'g> {
+    fn new<'a>(graph: &'a FlowNetwork, s: usize, t: usize) -> FordFulkerson<'a> {
+        graph.validate_vertex(s);
+        graph.validate_vertex(t);
+
+        let n = graph.v();
+        let mut ret = FordFulkerson {
+            graph: graph,
+            marked: iter::repeat(false).take(n).collect(),
+            edge_to: iter::repeat(None).take(n).collect(),
+            value: 0.0
+        };
+        ret.ford_fulkerson(s, t);
+        ret
+    }
+
+    fn ford_fulkerson(&mut self, s: usize, t: usize) {
+        // if !self.is_feasible(s, t) {
+        //     panic!("initial flow is infeasible");
+        // }
+
+        self.value = self.excess(t);
+        while self.has_augmenting_path(s, t) {
+            let mut bottle = f64::INFINITY;
+            let mut v = t;
+            while v != s {
+                let ee = self.edge_to[v].as_ref().unwrap().clone();
+                bottle = bottle.min(ee.write().unwrap().residual_capacity_to(v));
+                v = ee.read().unwrap().other(v);
+            }
+
+            // augment flow
+            let mut v = t;
+            while v != s {
+                let ee = self.edge_to[v].as_ref().unwrap().clone();
+                ee.write().unwrap().add_residual_flow_to(v, bottle);
+                v = ee.read().unwrap().other(v);
+            }
+
+            self.value += bottle;
+            println!("233 value=> {}", self.value);
+        }
+
+    }
+
+    pub fn value(&self) -> f64 {
+        self.value
+    }
+
+    // is v in the s side of the min s-t cut?
+    pub fn in_cut(&self, v: usize) -> bool {
+        self.graph.validate_vertex(v);
+        self.marked[v]
+    }
+
+    // is there an augmenting path?
+    fn has_augmenting_path(&mut self, s: usize, t: usize) -> bool {
+        println!("calling s={} t={}", s, t);
+        self.edge_to = iter::repeat(None).take(self.graph.v()).collect();
+        self.marked = iter::repeat(false).take(self.graph.v()).collect();
+
+        let mut queue = Queue::<usize>::new();
+        queue.enqueue(s);
+        self.marked[s] = true;
+        while !queue.is_empty() && !self.marked[t] {
+            let v = queue.dequeue().unwrap();
+
+            for ee in self.graph.adj(v) {
+                let e = ee.read().unwrap();
+                let w = e.other(v);
+
+                // if residual capacity from v to w
+                if e.residual_capacity_to(w) > 0.0 {
+                    if !self.marked[w] {
+                        self.edge_to[w] = Some(ee.clone());
+                        self.marked[w] = true;
+                        queue.enqueue(w);
+                    }
+                }
+            }
+        }
+        self.marked[t]
+    }
+
+    // return excess flow at vertex v
+    fn excess(&self, v: usize) -> f64 {
+        let mut excess = 0f64;
+        for e in self.graph.adj(v) {
+            let e = e.read().unwrap();
+            if v == e.from() {
+                excess -= e.flow();
+            } else {
+                excess += e.flow();
+            }
+        }
+        excess
+    }
+
+    fn is_feasible(&self, s: usize, t: usize) -> bool {
+        const EPSILON: f64 = 1e-11;
+
+        // check that capacity constraints are satisfied
+        for v in 0 .. self.graph.v() {
+            for e in self.graph.adj(v) {
+                let e = e.read().unwrap();
+                if e.flow() < -EPSILON || e.flow() > e.capacity() + EPSILON {
+                    panic!("edge does not satisfy capacity constraints");
+                    // return false;
+                }
+            }
+        }
+
+        // check that net flow into a vertex equals zero, except at source and sink
+        if (self.value + self.excess(s)).abs() > EPSILON {
+            panic!("Excess at source = {}", self.excess(s));
+            // return false;
+        }
+
+        for v in 0 .. self.graph.v() {
+            if v == s || v == t {
+                continue;
+            } else {
+                if self.excess(v).abs() > EPSILON {
+                    panic!("net flow out of {:?} doesn't equal zero", v);
+                    // return false;
+                }
+            }
+        }
+        return true;
+    }
+}
+
+impl FlowNetwork {
+    pub fn ford_fulkerson<'a>(&'a self, s: usize, t: usize) -> FordFulkerson<'a> {
+        assert!(s != t, "source equal to sink");
+        FordFulkerson::new(self, s, t)
+    }
+}
+
+
+#[test]
+fn test_flow_network() {
+    let mut g = FlowNetwork::new(6);
+
+    g.add_edge(FlowEdge::new(0, 1, 2.0));
+    g.add_edge(FlowEdge::new(0, 2, 3.0));
+    g.add_edge(FlowEdge::new(1, 3, 3.0));
+    g.add_edge(FlowEdge::new(1, 4, 1.0));
+    g.add_edge(FlowEdge::new(2, 3, 1.0));
+    g.add_edge(FlowEdge::new(2, 4, 1.0));
+    g.add_edge(FlowEdge::new(3, 5, 2.0));
+    g.add_edge(FlowEdge::new(4, 3, 3.0));
+
+    println!("=> \n{}", g.to_dot());
+
+    let maxflow = g.ford_fulkerson(0, 5);
+
 }
