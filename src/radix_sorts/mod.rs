bb5e2ae4af27abb45b4001d1dd97e82c6efdb0b9
andelf <andelf@gmail.com>
2015-08-20 15:01:57 +0800

    radix_sorts: add MSD sort

--- src/radix_sorts/mod.rs
+++ src/radix_sorts/mod.rs
@@ -147,3 +147,122 @@ fn test_lsd_ints_sort() {
     assert!(is_sorted(&to_be_sorted));
 
 }
+
+
+const CUTOFF: usize = 15;
+
+pub trait MSD: Clone + Default + PartialOrd {
+    fn r() -> usize;
+    fn width(&self) -> usize;
+    fn item_at(&self, n: usize) -> usize;
+
+    fn insertion(a: &mut [Self], lo: usize, hi: usize) {
+        for i in lo .. hi+1 {
+            for j in (lo .. i).rev() {
+                if !(j > lo && a[j] < a[j-1]) {
+                    break;
+                }
+                a.swap(j, j-1);
+            }
+        }
+    }
+
+    fn sort_helper(a: &mut [Self], lo: usize, hi: usize, d: usize, aux: &mut [Self]) {
+        if hi <= lo + CUTOFF {
+            Self::insertion(a, lo, hi);
+            return;
+        }
+
+        let n = a.len();
+        let r = Self::r();
+
+        // compute frequency counts (need R = 256)
+        let mut count = iter::repeat(0).take(r+1).collect::<Vec<usize>>();
+        for i in 0 .. n {
+            count[a[i].item_at(d) + 1] += 1;
+        }
+
+        // transform counts to indicies
+        for r in 0 .. r {
+            count[r+1] += count[r];
+        }
+
+        // distribute
+        for i in lo .. hi+1 {
+            aux[count[a[i].item_at(d)]] = a[i].clone();
+            count[a[i].item_at(d)] += 1;
+        }
+
+        // copy back
+        for i in lo .. hi+1 {
+            a[i] = aux[i-lo].clone();
+        }
+
+        // no more bits
+        if d == a[0].width() {
+            return ;
+        }
+
+        if count[0] > 0 {
+            Self::sort_helper(a, lo, lo + count[0] - 1, d+1, aux);
+        }
+        for r in 0 .. r {
+            if count[r+1] > count[r] {
+                Self::sort_helper(a, lo + count[r], lo + count[r+1], d+1, aux);
+            }
+        }
+    }
+
+    fn sort(a: &mut [Self]) {
+        let n = a.len();
+        let mut aux = iter::repeat(Default::default()).take(n).collect::<Vec<Self>>();
+
+        Self::sort_helper(a, 0, n-1, 0, &mut aux);
+    }
+}
+
+
+impl MSD for u32 {
+    fn r() -> usize { 256 }
+    fn width(&self) -> usize { 4 }
+    fn item_at(&self, n: usize) -> usize {
+        // 0:   lowest byte
+        // n-1: highest byte
+        ((self >> (8 * n)) & 0xFF) as usize
+    }
+}
+
+impl MSD for i16 {
+    fn r() -> usize { 256 }
+    fn width(&self) -> usize { 2 }
+    fn item_at(&self, n: usize) -> usize {
+        // 0:   lowest byte
+        // n-1: highest byte
+        let mut ret = (self >> (8 * n)) & 0xFF;
+        // handle highest byte negative
+        if n == 1 {
+            if *self < 0 {
+                ret -= 128;
+            } else {
+                ret += 128;
+            }
+        }
+        ret as usize
+    }
+}
+
+
+#[test]
+fn test_msd_ints_sort() {
+    extern crate rand;
+    use rand::{thread_rng, Rng};
+
+    let mut rng = thread_rng();
+    for sz in vec![20, 1000] {
+        let mut array = rng.gen_iter().take(sz).collect::<Vec<u32>>();
+        assert!(!is_sorted(&array));
+        LSD::sort(&mut array);
+        assert!(is_sorted(&array));
+    }
+
+}
