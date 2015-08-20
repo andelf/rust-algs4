8b9ef43ca1a6301cb57ce13caf28d5dbd30b9e2f
andelf <andelf@gmail.com>
2015-08-20 16:16:18 +0800

    radix_sorts: fix bug in radix msd sort; refactor LSD/MSD into RadixSort trait

--- src/radix_sorts/mod.rs
+++ src/radix_sorts/mod.rs
@@ -35,12 +35,16 @@ pub fn lsd_string_sort(a: &mut [&str]) {
     }
 }
 
-pub trait LSD: Clone + Default {
+
+const CUTOFF: usize = 15;
+
+pub trait RadixSort: Clone + Default + PartialOrd + ::std::fmt::Debug {
     fn r() -> usize;
     fn width(&self) -> usize;
     fn item_at(&self, n: usize) -> usize;
 
-    fn sort(a: &mut [Self]) {
+    // sort algorithms
+    fn lsd_sort(a: &mut [Self]) {
         let w = a[0].width();
         // assume all width equals
         let n = a.len();
@@ -75,11 +79,87 @@ pub trait LSD: Clone + Default {
             }
         }
     }
+
+    fn msd_insertion(a: &mut [Self], lo: usize, hi: usize, _d: usize) {
+        for i in lo .. hi+1 {
+            for j in (0 .. i+1).rev() {
+                // if !(j > lo && a[j].item_at(d) < a[j-1].item_at(d)) {
+                if !(j > lo && a[j] < a[j-1]) {
+                    break;
+                }
+                a.swap(j, j-1);
+            }
+        }
+    }
+
+    fn msd_sort_helper(a: &mut [Self], lo: usize, hi: usize, d: usize, aux: &mut [Self]) {
+        if hi <= lo + CUTOFF {
+            Self::msd_insertion(a, lo, hi, d);
+            return;
+        }
+
+        let r = Self::r();
+
+        // compute frequency counts (need R = 256)
+        let mut count = iter::repeat(0).take(r+1).collect::<Vec<usize>>();
+        for i in lo .. hi+1 {
+            count[a[i].item_at(d) + 1] += 1;
+        }
+
+        // transform counts to indicies
+        for r in 0 .. r {
+            count[r+1] += count[r];
+        }
+
+        // distribute
+        for i in lo .. hi+1 {
+            aux[count[a[i].item_at(d)]] = a[i].clone();
+            count[a[i].item_at(d)] += 1;
+        }
+
+        // copy back
+        for i in lo .. hi+1 {
+            a[i] = aux[i-lo].clone();
+        }
+
+        // no more bits
+        if d == 0 {
+            return ;
+        }
+
+        if count[0] > 1 {
+            Self::msd_sort_helper(a, lo, lo + count[0] - 1, d-1, aux);
+        }
+        for r in 0 .. r {
+            if count[r+1] > count[r] {
+                Self::msd_sort_helper(a, lo + count[r], lo + count[r+1] - 1, d-1, aux);
+            }
+        }
+    }
+
+    fn msd_sort(a: &mut [Self]) {
+        let n = a.len();
+        let w = a[0].width();
+        let mut aux = iter::repeat(Default::default()).take(n).collect::<Vec<Self>>();
+        Self::msd_sort_helper(a, 0, n-1, w-1, &mut aux);
+    }
+
 }
 
-impl LSD for u32 {
+impl RadixSort for u32 {
     fn r() -> usize { 256 }
     fn width(&self) -> usize { 4 }
+    fn item_at(&self, n: usize) -> usize {
+        // 0:   lowest byte
+        // n-1: highest byte
+        // ((self >> (8 * (4-n))) & 0xFF) as usize
+        ((self >> (8 * n)) & 0xFF) as usize
+    }
+}
+
+impl RadixSort for u16 {
+    fn r() -> usize { 256 }
+    fn width(&self) -> usize { 2 }
     fn item_at(&self, n: usize) -> usize {
         // 0:   lowest byte
         // n-1: highest byte
@@ -87,7 +167,7 @@ impl LSD for u32 {
     }
 }
 
-impl LSD for i16 {
+impl RadixSort for i16 {
     fn r() -> usize { 256 }
     fn width(&self) -> usize { 2 }
     fn item_at(&self, n: usize) -> usize {
@@ -135,7 +215,7 @@ fn test_lsd_ints_sort() {
         2837710927, 560466804, 2778567425, 265098057, 18593204];
 
     assert!(!is_sorted(&to_be_sorted));
-    LSD::sort(&mut to_be_sorted);
+    RadixSort::lsd_sort(&mut to_be_sorted);
     assert!(is_sorted(&to_be_sorted));
 
     let mut to_be_sorted: Vec<i16> = vec![
@@ -143,112 +223,8 @@ fn test_lsd_ints_sort() {
         2318, 9647, -7906, -8452, -127, 128, 127];
 
     assert!(!is_sorted(&to_be_sorted));
-    LSD::sort(&mut to_be_sorted);
+    RadixSort::lsd_sort(&mut to_be_sorted);
     assert!(is_sorted(&to_be_sorted));
-
-}
-
-
-const CUTOFF: usize = 15;
-
-pub trait MSD: Clone + Default + PartialOrd {
-    fn r() -> usize;
-    fn width(&self) -> usize;
-    fn item_at(&self, n: usize) -> usize;
-
-    fn insertion(a: &mut [Self], lo: usize, hi: usize) {
-        for i in lo .. hi+1 {
-            for j in (lo .. i).rev() {
-                if !(j > lo && a[j] < a[j-1]) {
-                    break;
-                }
-                a.swap(j, j-1);
-            }
-        }
-    }
-
-    fn sort_helper(a: &mut [Self], lo: usize, hi: usize, d: usize, aux: &mut [Self]) {
-        if hi <= lo + CUTOFF {
-            Self::insertion(a, lo, hi);
-            return;
-        }
-
-        let n = a.len();
-        let r = Self::r();
-
-        // compute frequency counts (need R = 256)
-        let mut count = iter::repeat(0).take(r+1).collect::<Vec<usize>>();
-        for i in 0 .. n {
-            count[a[i].item_at(d) + 1] += 1;
-        }
-
-        // transform counts to indicies
-        for r in 0 .. r {
-            count[r+1] += count[r];
-        }
-
-        // distribute
-        for i in lo .. hi+1 {
-            aux[count[a[i].item_at(d)]] = a[i].clone();
-            count[a[i].item_at(d)] += 1;
-        }
-
-        // copy back
-        for i in lo .. hi+1 {
-            a[i] = aux[i-lo].clone();
-        }
-
-        // no more bits
-        if d == a[0].width() {
-            return ;
-        }
-
-        if count[0] > 0 {
-            Self::sort_helper(a, lo, lo + count[0] - 1, d+1, aux);
-        }
-        for r in 0 .. r {
-            if count[r+1] > count[r] {
-                Self::sort_helper(a, lo + count[r], lo + count[r+1], d+1, aux);
-            }
-        }
-    }
-
-    fn sort(a: &mut [Self]) {
-        let n = a.len();
-        let mut aux = iter::repeat(Default::default()).take(n).collect::<Vec<Self>>();
-
-        Self::sort_helper(a, 0, n-1, 0, &mut aux);
-    }
-}
-
-
-impl MSD for u32 {
-    fn r() -> usize { 256 }
-    fn width(&self) -> usize { 4 }
-    fn item_at(&self, n: usize) -> usize {
-        // 0:   lowest byte
-        // n-1: highest byte
-        ((self >> (8 * n)) & 0xFF) as usize
-    }
-}
-
-impl MSD for i16 {
-    fn r() -> usize { 256 }
-    fn width(&self) -> usize { 2 }
-    fn item_at(&self, n: usize) -> usize {
-        // 0:   lowest byte
-        // n-1: highest byte
-        let mut ret = (self >> (8 * n)) & 0xFF;
-        // handle highest byte negative
-        if n == 1 {
-            if *self < 0 {
-                ret -= 128;
-            } else {
-                ret += 128;
-            }
-        }
-        ret as usize
-    }
 }
 
 
@@ -258,11 +234,15 @@ fn test_msd_ints_sort() {
     use rand::{thread_rng, Rng};
 
     let mut rng = thread_rng();
-    for sz in vec![20, 1000] {
+    for sz in vec![6, 20, 1000] {
+        let mut array = rng.gen_iter().take(sz).collect::<Vec<i16>>();
+        assert!(!is_sorted(&array));
+        RadixSort::msd_sort(&mut array);
+        assert!(is_sorted(&array));
+
         let mut array = rng.gen_iter().take(sz).collect::<Vec<u32>>();
         assert!(!is_sorted(&array));
-        LSD::sort(&mut array);
+        RadixSort::msd_sort(&mut array);
         assert!(is_sorted(&array));
     }
-
 }
